d7b7c9cce5fec7470767233176d5968dc3c8a11e
Artem <dizik93@gmail.com>
2022-11-07 00:21:33 +0100

    add style for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,20 @@
-import CategoryList from "./components/CategoryList/CategoryList";
+/* eslint-disable react/react-in-jsx-scope */
+import { Route, Routes } from "react-router-dom";
+import Home from "./routes/Home/Home";
+import Navigation from "./routes/Navigation/Navigation"
 
+
+const Shop = () => {
+  return <h1>Shop page test</h1>;
+};
 function App() {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
   return (
-    <CategoryList data={categories}/>
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
   );
 }
 
